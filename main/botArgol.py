acad1a8baa9776ee724bf08f605aa254ba60f2be
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2022-11-13 22:57:48 -0300

    agregamos rellaborales y varios bug

--- main/botArgol.py
+++ main/botArgol.py
@@ -7,73 +7,71 @@ import xlrd
 import glob
 
 def reldata():
-    # folder_path = '../data/relaborales' Aca sube dos niveles
-    folder_path = '../data/relaborales'
-    file_list = glob.glob(folder_path + "/*.txt")
-    print(len(file_list))
-    main_dataframe = pd.DataFrame(pd.read_fwf(file_list[0], widths=[12, 60, 160,100,59,60]))
-   
-    for i in range(1,len(file_list)):
-      data = pd.read_fwf(file_list[i], widths=[12, 60, 160,100,59,60]) #, skiprows=1)        
-      df = pd.DataFrame(data)
-      main_dataframe = pd.concat([main_dataframe,df], axis = 0)
+# folder_path = '../data/relaborales' Aca sube dos niveles
+#folder_path = '../data/relaborales'
+ i = 0
+ apellido = nombre = cuil = obrasocial = modsicos = acteconomica = convenio = categoria = puesto = salario = ingreso = cct = califprof = ""
+ var = 1
+ main_dataframe = pd.DataFrame()
 
-    name = main_dataframe["APELLIDO Y NOMBRE"].str.split(expand=True)
-    nrocolum= len(name.columns)
-    print(name.columns.values)
-    if nrocolum == 3:
-    #name.set_axis(['last_name0', 'last_name1', 'first_0', 'first_1', 'first_2'], axis=1)
-    #name.columns = ['last_name0', 'last_name1', 'first_0', 'first_1', 'first_2']
-        name['APELLIDO'] = name[0] 
-        name['NOMBRE'] = name[1].str.cat(name[2], sep = " ", na_rep = "")
-        name.drop([0,1,2], axis = 'columns', inplace=True)
-    #name['NOMBRE'] = name.NOMBRE.str.cat(name[1], sep = " ", na_rep = "")
-    #main_dataframe = pd.concat([main_dataframe, name['NOMBRE'],name['APELLIDO']  ], axis=1)
-    #print(name)
-    #main_dataframe = main_dataframe.reindex(columns=['Grupo','Fecha','Tipo','Punto de Venta'])
-    else :
-         name['APELLIDO'] = name[0] 
-         name['NOMBRE'] = name[1].str.cat(name[2], sep = " ", na_rep = "")
-         name['NOMBRE'] = name.NOMBRE.str.cat(name[3], sep = " ", na_rep = "")
-         name.drop([0,1,2,3], axis = 'columns', inplace=True)
+ main_dataframe = pd.DataFrame(columns=['GRUPO', 'Nº LEGAJO', 'APELLIDO', 'NOMBRE', 'FECHA DE NACIMIENTO DD/MM/AAAA', 'CUIL', 'FECHA DE INGRESO', 'OBRA SOCIAL', 'SITUACION DE REVISTA', 'ACTIVIDAD ECONOMICA A LA QUE SE ENCUENTRA AFECTADO', 'REMUNERACION MENSUAL', 'INCLUIDO EN CCT', 'CONVENIO APLICABLE', 'CATEGORIA', 'SECCION', 'CALIFICACION PROFESIONAL', 'PUESTO DESEMPEÑADO (Resolución SRT 244/06 Anexo II)', 'CODIGO ACTIVIDAD SICOSS', 'CONDICION SICOSS', 'MODALIDAD DE CONTRATACION SICOSS', 'CODIGO DESCRIPCION DE PUESTO DE TRABAJO'])
+ with open('../data/relaborales/labodata.txt', "r") as f:
+    for linea in f:        
+               
+        if i == 1:
+            x = linea.split(" ")
+            apellido = x[1]
+            if len(x) >= 4:
+                 nombre = x[2] + " " + x[3]
+            else:
+                 nombre = x[2]
+            cuil = linea[9:22]
+            cuil = cuil.replace("-", "")
+        if i == 2:            
+            x = linea.split(" ")
+            obrasocial = x[1]
+            obrasocial = obrasocial[-6:]
+        if i == 3:
+            modsicos = linea.split("-")
+            modsicos = modsicos[0]
+            modsicos = modsicos[14:17]
+        if i == 5:
+            x = linea.split(" ")
+            acteconomica = x[0]
+            acteconomica = acteconomica[10:16]
+                        
+        if i == 6:            
+            convenio = linea.replace("Convenio:", "")
+            if "9999/99" in convenio:
+                cct = "NO"
+            else:
+                cct = "SI"
+        if i == 7:
+            categoria = linea.split(" ")[0]
+            categoria = categoria[11:17]
+        if i == 8:
+            puesto = linea.split(" ")[0]
+            puesto = puesto[7:11]
+            califprof = linea.split("-")[1]
+        if i == 10:
+            x = linea.split(" ")
+            salario = x[1]
+            salario = salario[8:15]            
+            
+        if i == 13:
+            ingreso = linea[-11:]
+        if i == 20:
+            i = 0
+            main_dataframe = main_dataframe.append({'APELLIDO': apellido, 'NOMBRE':nombre, 'CUIL': cuil,'OBRA SOCIAL': obrasocial, 'MODALIDAD DE CONTRATACION SICOSS': modsicos,'ACTIVIDAD ECONOMICA A LA QUE SE ENCUENTRA AFECTADO': acteconomica,'SITUACION DE REVISTA': var,'CONVENIO APLICABLE': convenio, 'CATEGORIA':categoria, 'PUESTO DESEMPEÑADO (Resolución SRT 244/06 Anexo II)': puesto,'REMUNERACION MENSUAL': salario, 'FECHA DE INGRESO': ingreso, "INCLUIDO EN CCT": cct , "CALIFICACION PROFESIONAL": califprof, "SECCION" : califprof}, ignore_index=True)
+         
         
-    main_dataframe.insert(0,'GRUPO',"", allow_duplicates=False)
-    main_dataframe.insert(1,'Nº LEGAJO',"", allow_duplicates=False)
-   # main_dataframe = main_dataframe.reindex(columns=['GRUPO','Nº LEGAJO','APELLIDO','NOMBRE'])
-    main_dataframe.insert(2,'APELLIDO',"", allow_duplicates=False)
-    main_dataframe['APELLIDO'] = name['APELLIDO']
-    main_dataframe.insert(3,'NOMBRE',"", allow_duplicates=False)
-    main_dataframe['NOMBRE'] = name['NOMBRE']
-    main_dataframe.drop(['APELLIDO Y NOMBRE'], axis = 'columns', inplace=True)
-    
+        i = i + 1
+ main_dataframe.to_excel("../data/formato_sec_relaborales.xlsx", index=False) 
 
-    main_dataframe.insert(4,'FECHA DE NACIMIENTO DD/MM/AAAA',"", allow_duplicates=False)
-    #main_dataframe.insert(5,'Nº LEGAJO',"", allow_duplicates=False)
-    main_dataframe.insert(6,'FECHA DE INGRESO',"", allow_duplicates=False)
-    #main_dataframe.insert(7,'OBRA SOCIAL',"", allow_duplicates=False)
-    main_dataframe.insert(8,'SITUACION DE REVISTA',"1", allow_duplicates=False)
-    main_dataframe.insert(9,'ACTIVIDAD ECONÓMICA A LA QUE SE ENCUENTRA AFECTADO',"", allow_duplicates=False)
-    main_dataframe['ACTIVIDAD ECONÓMICA A LA QUE SE ENCUENTRA AFECTADO'] = main_dataframe['ACTIVIDAD LABORAL']
-    main_dataframe.drop(['ACTIVIDAD LABORAL'], axis = 'columns', inplace=True)
-
-    main_dataframe.insert(10,'REMUNERACION MENSUAL',"", allow_duplicates=False) #	
-    main_dataframe.insert(11,'INCLUIDO EN CCT',"SI", allow_duplicates=False)
-    main_dataframe.insert(12,'CONVENIO APLICABLE',"", allow_duplicates=False)
-    main_dataframe.insert(13,'CATEGORIA',"", allow_duplicates=False)
-    main_dataframe.insert(14,'SECCION',"", allow_duplicates=False)
-    main_dataframe.insert(15,'CALIFICACION PROFESIONAL',"", allow_duplicates=False)
-    main_dataframe.insert(16,'PUESTO DESEMPEÑADO (Resolución SRT 244/06 Anexo II)',"", allow_duplicates=False)
-    main_dataframe['PUESTO DESEMPEÑADO (Resolución SRT 244/06 Anexo II)'] = main_dataframe['PUESTO DESEMP.']
-    main_dataframe.drop(['PUESTO DESEMP.'], axis = 'columns', inplace=True)
-    main_dataframe.insert(17,'CODIGO ACTIVIDAD SICOSS',"", allow_duplicates=False)
-    main_dataframe.insert(18,'CONDICION SICOSS',"", allow_duplicates=False)
-    main_dataframe.insert(19,'MODALIDAD DE CONTRATACION SICOSS',"", allow_duplicates=False)
-    main_dataframe.insert(20,'CODIGO DESCRIPCION DE PUESTO DE TRABAJO',"", allow_duplicates=False)
-
-    writer = ExcelWriter('../data/formato_sec_rel_laborales.xlsx')
-    main_dataframe.to_excel(writer, 'Tabla Relaciones Laborales', index=False)
-    writer.close()
-    messagebox.showinfo(message="¡Haz generado la tabla de relaciones laborales!", title="Finalizado")
+ #writer = ExcelWriter('../data/formato_sec_rel_laborales.xlsx')
+ #main_dataframe.to_excel(writer, 'Tabla Relaciones Laborales', index=False)
+ #writer.close()
+ messagebox.showinfo(message="¡Haz generado la tabla de relaciones laborales!", title="Finalizado")
 
 def cleandata():
       
@@ -112,9 +110,8 @@ def cleandata():
     main_dataframe.insert(0,'Grupo','') 
 #header = main_dataframe.head(1)
     main_dataframe.replace({"$": "PESOS", "USD": "DÓLAR" , "€" : "EURO" , "$R" : "REAL"}, inplace=True)
-    main_dataframe['Tipo'] = main_dataframe.Tipo.apply(
-    lambda x: x.split(' ')[0]
-    )
+    main_dataframe['Tipo'] = main_dataframe.Tipo.apply(lambda x: x.split(' ')[0] )
+    main_dataframe['Tipo'] = main_dataframe['Tipo'].astype('int64')
 
     datos = (main_dataframe['Imp. Neto Gravado']+main_dataframe['Imp. Neto No Gravado']+main_dataframe['Imp. Op. Exentas'])*main_dataframe['Tipo de Cambio del Comprobante']
     imp_total = main_dataframe['MONTO TOTAL DEL COMPROBANTE EN PESOS']*main_dataframe['Tipo de Cambio del Comprobante']
